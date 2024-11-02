c9fd88f3f13f704dd43be1e85d55887c7e55f7f8
youssefemad25 <124690632+youssefemad25@users.noreply.github.com>
2024-11-02 23:36:41 +0200

    Update Module1.v

--- Modules/Module1.v
+++ Modules/Module1.v
@@ -9,8 +9,8 @@ wire w;
 wire y;
 wire n;
         
-not #(2) g1 (n,a);    
-and #(5) g0 (w,c,n);
+not #(2) g0 (n,a);    
+and #(5) g1 (w,c,n);
 or #(9) g2 (y,a,b);
-and #(5) g0 (o,y,w);
+and #(5) g3 (o,y,w);
 endmodule;
